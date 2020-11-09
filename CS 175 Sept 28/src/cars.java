ef2588f834bfa49dd3ceaa58ed94b13fb50b6b29
AnnaNardelli <AnnaNardelli@10.71.11.130>
2020-11-09 11:35:48 -0500

    CS 175 Sept 28

---
+++ CS 175 Sept 28/src/cars.java
@@ -0,0 +1,21 @@
+public class cars {
+
+public void color (String objName) {
+	System.out.println (objName);
+	
+public void make (objName) {
+	
+public static void main (String []args) {
+	Cars objName = new cars ();
+	objName.color("blue");
+	objName.make("Honda");
+	
+	Cars obj2 = new cars ();
+	obj2.color("red");
+	obj2.make("Ford");
+}
+}
+
+}
+
+}
