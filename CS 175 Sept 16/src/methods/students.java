a43a2562e04b499b700dd6ea9c1ac00e7df40d57
AnnaNardelli <AnnaNardelli@10.71.11.130>
2020-11-09 11:33:41 -0500

    CS 175 Sept 16

---
+++ CS 175 Sept 16/src/methods/students.java
@@ -0,0 +1,24 @@
+package methods;
+
+public class students {
+
+public void student(String objName)   {
+	System.out.print(objName);
+}
+public void student1() {
+	String school = "Monmouth University";
+	String activity = "Basketball";
+} 
+
+public void student2 () {
+	String school= "Monmouth University";
+	String activity= "Golf";
+	
+}
+public void student3 () {
+	String school="Rutgers University";
+	String activity="Debate Team"
+	
+}
+}
+//this is all wrong, supposed to be objects
