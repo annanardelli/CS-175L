945d48811f59bf36b969e95ca3601df69a603ddd
AnnaNardelli <AnnaNardelli@macbook-pro.lan>
2020-11-30 19:19:27 -0500

    Pyramid

---
+++ Pyramid/src/Pyramid.java
@@ -0,0 +1,23 @@
+
+public class Pyramid {
+
+	public static void main (String []args) {
+		for (int i=1; i<=1;  i++) {
+			System.out.println("    .  ");
+			for (int j=1; j<=1; j++) {
+				System.out.println("   ... ");
+				for(int l=1; l<=1; l++) {
+					System.out.println("  .....");
+					for(int h=1; h<=1; h++) {
+						System.out.println(" .......");
+							for(int a=1; a<=1; a++) {
+								System.out.println(".........");
+							}
+				}
+			}
+		}
+		
+
+	}
+}
+}
