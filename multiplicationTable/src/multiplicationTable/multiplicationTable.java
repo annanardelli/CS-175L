b303e5c0df9ab986cbb99103e141952f1bbcf93c
AnnaNardelli <AnnaNardelli@macbook-pro.lan>
2020-12-07 15:49:20 -0500

    Multiplication Table

---
+++ multiplicationTable/src/multiplicationTable/multiplicationTable.java
@@ -0,0 +1,18 @@
+package multiplicationTable;
+
+public class multiplicationTable {
+	public static void main (String[] args) {
+		int a;
+		int b;
+		for (a=1; a<=10; a++)
+		{
+		    for (b=1; b<=10; b++)
+		    {
+		        System.out.printf("%4d", a*b);  
+		    }
+		    System.out.println("");
+		}    
+	
+
+}
+}
