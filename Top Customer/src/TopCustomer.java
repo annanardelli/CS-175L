508b38ff9a44ce6253e659d77e73ae6b3780df0b
annanardelli <60702479+annanardelli@users.noreply.github.com>
2021-03-29 01:30:30 -0400

    TopCustomer

--- Top Customer/src/TopCustomer.java
+++ Top Customer/src/TopCustomer.java
@@ -1,3 +1,7 @@
+/**
+ * This program uses two ArrayLists to determine and then print
+ * the name of the top customer at a store.
+ */
 import java.util.ArrayList;
 import java.util.*;
 import java.util.Scanner;
@@ -6,11 +10,14 @@ public class TopCustomer {
 	public static void main (String[] args) {
 		Scanner input = new Scanner(System.in);
 		Scanner strings = new Scanner(System.in);
+		
+		//Creates the two ArrayLists
 		ArrayList <String> customers = new ArrayList<String> ();
 		ArrayList <Double> prices = new ArrayList<Double> ();
 		System.out.print("Enter the first price: ");
+		
+		//takes input from user and sends the ArrayLists to the nameOfBestCustomer() method
 		while (input.hasNextDouble()) {
-			//int counter = 0;
 			double price = input.nextDouble();
 			if (price == 0) {
 				TopCustomer bestCust= new TopCustomer();
@@ -22,22 +29,16 @@ public class TopCustomer {
 			String name = strings.nextLine();
 			customers.add(name);
 			System.out.println("Enter the price or 0 to end: ");
-			//counter++;
 		}
 		strings.close();
 		input.close();
 	}
 	public String nameOfBestCustomer(ArrayList<String> customers, ArrayList<Double> prices) {
+		//finds the max price
 			double maxNumber= Collections.max(prices);
-			//int i =0;
+		//finds the index of the max price in the ArrayList
 			int index= prices.indexOf(maxNumber);
-			//for (double price: prices) {
-				//if (price != maxNumber) {
-					 // i++;
-				//}
-				//else {
-					//break;
-				//}
+		//finds the matching name in the other ArrayList
 			String name = customers.get(index);
 			return name;
 		}	
